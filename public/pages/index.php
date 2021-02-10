aaa2724858c301875cbb5348d09001ddebffa95f
Rafa Patino <RafaPatino01@outlook.com>
2021-02-10 16:18:48 -0600

    Changed .html to .php

---
+++ public/pages/index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Arction Movements | Inicio</title>
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="author" content="" />
+		<link rel="shortcut icon" href=""> 
+
+		<!-- BOOTSTRAP CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="../css/main.css">
+
+		 <!--CSS GRIDS-->
+		<link rel="stylesheet" type="text/css" href="../css/collage.css">
+
+	</head>
+	<body>
+		<div class="header bg-light py-2">
+		    <div class="container mt-2">
+		        <div class="row">
+		            <div class="col-6">
+		                <a href="contacto.php">CONTACTO</a>
+		            </div>
+		            <div class="col-6 text-right">
+		                <span class="mr-2"><a href="enviar_material.php">ENVIAR MATERIAL</a> </span> | <span class="ml-2"> <a href="registro.php">INICIAR SESIÓN</a></span>
+		            </div>
+		        </div>
+					<div class="text-center">
+					<a href="index.php">
+						<img class="mt-4 logo" src="../images/logo.png">
+					</a>
+					</div>
+		    </div>
+		</div>
+		<nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light underline">
+		    <div class="container">
+		    	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar1">
+            <span class="navbar-toggler-icon"></span>
+        		</button>
+		        <div class="collapse navbar-collapse justify-content-center" id="navbar1">
+
+		        	<a class="navbar-brand" href="../pages/index.php">
+				    	<img id="navbar-logo" src="../images/small_logo.png" alt="Logo">
+				  	</a>
+
+		            <ul class="navbar-nav">
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="acerca_de.php">Acerca de</a>
+		                </li>
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="proyectos.php">Proyectos & Obras</a>
+		                </li>
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="#">Investigación</a>
+		                </li>
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="#">Red de trabajo</a>
+		                </li>
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="#">Eventos</a>
+		                </li>
+		                <li class="nav-item m-3 lead active">
+		                    <a class="nav-link" href="#">Tienda</a>
+		                </li>
+		            </ul>
+		        </div>
+		    </div>
+		</nav>
+
+		<div class="container mb-5 mt-3">
+			<div class="wrapper">
+				
+				<div class="box a hover_img_big">  
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/900x500" alt="Card image">
+						<div class="card-img-overlay overlay-green d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h1 class="card-text font-weight-bold">Proyecto del mes</h1>
+							  <p class="card-text lead">Puebla, Mex.</p>
+							</div>
+						</div>
+					</div>
+				</div> <!--A-->
+
+				<div class="box b hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/400x250" alt="Card image">
+						<div class="card-img-overlay overlay-red d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h1 class="card-text font-weight-bold">Acerca de</h1>
+							</div>
+						</div>
+					</div>
+				</div> <!--B-->
+
+				<div class="box c hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/600x720">
+						<div class="card-img-overlay overlay-blue d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h1 class="card-text font-weight-bold">Convocatoria</h1>
+							</div>
+						</div>
+					</div>
+				</div> <!--c-->
+
+				<div class="box d hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/400x540">
+						<div class="card-img-overlay overlay-orange d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h2 class="card-text font-weight-bold">Evento del mes</h2>
+							</div>
+						</div>
+					</div>
+				</div> <!--D-->
+				<div class="box e hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/400x300">
+						<div class="card-img-overlay overlay-orange d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h2 class="card-text font-weight-bold">Investigación</h2>
+							</div>
+						</div>
+					</div>
+				</div> <!--e-->
+				<div class="box f hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/400x300">
+						<div class="card-img-overlay overlay-pink d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h2 class="card-text font-weight-bold">Tienda</h2>
+							</div>
+						</div>
+					</div>
+				</div> <!--f-->
+				<div class="box g hover_img">
+					<div class="card text-white">
+						<img class="card-img" src="https://via.placeholder.com/400x314">
+						<div class="card-img-overlay overlay-green d-flex flex-column justify-content-end p-0">
+							<div class="text p-3">
+							  <h2 class="card-text font-weight-bold">Red de trabajo</h2>
+							</div>
+						</div>
+					</div>
+				</div> <!--g-->
+				
+			</div>
+		</div>
+
+		<br>
+
+		<div class="container mt-5 mb-5">
+			<div class="row">
+				<div class="col-sm-4 mt-5">
+					<h1 class="mt-5 mr-5"><b>Mejorar el mundo a través de una arquitectura sustentable y pertinente.</b></h1>
+					<br>
+					<h3 class="text-muted mt-3 mr-5">Nuestra misión es recopilar proyectos, obras investigación y conectar personas que tengan el objetivo de mejorar una situación particular en el mundo.</h3>
+				</div>
+				<div class="col-sm-8 pl-5"><img src="../images/index/index.png" class="big_img"></div>
+			</div>
+		</div>
+		<footer class="bg-dark text-center text-lg-start mt-3 text-white">
+			<div class="text-center p-3 bg-black">
+				<a href="index.php">
+					<img class="mt-4 logo-footer" src="../images/dark_logo.png">
+				</a>
+		        <div class="container mt-5 mb-3">
+				  	<div class="row">
+					    <div class="col-sm">
+					      	<a href="acerca_de.php">Acerca de</a>
+					    </div>
+					    <div class="col-sm">
+					      	<a href="proyectos.php">Proyectos & obras</a>
+					    </div>
+					    <div class="col-sm">
+					      	<a href="">Investigación</a>
+					    </div>
+					    <div class="col-sm">
+					      	<a href="">Red de trabajo</a>
+					    </div>
+					    <div class="col-sm">
+					    	<a href="">Eventos</a>
+					    </div>
+					    <div class="col-sm">
+					    	<a href="">Tienda</a>
+					    </div>
+					    <div class="col-sm">
+					    	<a href="">Contacto</a>
+					    </div>
+				  	</div>
+				</div>
+			</div>
+			<div class="text-center p-3 bg-gray" style="letter-spacing: 5px;">
+				<small>TODOS LOS DERECHOS RESERVADOS</small>
+			</div>
+		</footer>
+
+
+		<!-- JQuery Popper and Bootstrap-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+		<!-- NAVBAR JQUERY-->
+		<script src="../js/navbar.js"></script>
+	</body>
+</html>
