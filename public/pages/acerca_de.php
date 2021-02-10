4fbea5a91af7ba73bc1a35e6dc8b66d3a12b32c0
Rafa Patino <RafaPatino01@outlook.com>
2021-02-10 17:10:04 -0600

    Fixed conflicts

--- public/pages/acerca_de.php
+++ public/pages/acerca_de.php
@@ -1,155 +1,10 @@
-<<<<<<< HEAD:public/pages/acerca_de.php
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>Arction Movements | Inicio</title>
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="author" content="" />
-		<link rel="shortcut icon" href=""> 
-
-		<!-- BOOTSTRAP CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-		<!-- CSS -->
-		<link rel="stylesheet" type="text/css" href="../css/main.css">
-
-		 <!--CSS GRIDS-->
-		<link rel="stylesheet" type="text/css" href="../css/collage.css">
-
-	</head>
-	<body>
-		<div class="header bg-light py-2">
-		    <div class="container mt-2">
-		        <div class="row">
-		            <div class="col-6">
-		                <a href="contacto.php">CONTACTO</a>
-		            </div>
-		            <div class="col-6 text-right">
-		                <span class="mr-2"><a href="enviar_material.php">ENVIAR MATERIAL</a> </span> | <span class="ml-2"> <a href="registro.php">INICIAR SESIÓN</a></span>
-		            </div>
-		        </div>
-					<div class="text-center">
-					<img class="mt-4 logo" src="../images/logo.png">
-					</div>
-		    </div>
-		</div>
-		<nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light underline">
-		    <div class="container">
-		    	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar1">
-            <span class="navbar-toggler-icon"></span>
-        		</button>
-		        <div class="collapse navbar-collapse justify-content-center" id="navbar1">
-
-		        	<a class="navbar-brand" href="../pages/index.php">
-				    	<img id="navbar-logo" src="../images/small_logo.png" alt="Logo">
-				  	</a>
-
-		            <ul class="navbar-nav">
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="acerca_de.php"><u>Acerca de</u></a>
-		                </li>
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="proyectos.php">Proyectos & Obras</a>
-		                </li>
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="#">Investigación</a>
-		                </li>
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="#">Red de trabajo</a>
-		                </li>
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="#">Eventos</a>
-		                </li>
-		                <li class="nav-item m-3 lead active">
-		                    <a class="nav-link" href="#">Tienda</a>
-		                </li>
-		            </ul>
-		        </div>
-		    </div>
-		</nav>
-
-		<div class="container mt-3">
-			<div class="row">
-				<div class="col-sm-5 mt-5">
-					<h1 class="mt-5 mr-5"><b>Mantente actualizado de las acciones y movimientos involucrados en la mejora colectiva</b></h1>
-					<br>
-					<p class="lead mt-3 mr-5">En nuestro sitio web encontrarás proyectos, obras, investigación y eventos que cubren temas de caracter social, económico y ambiental en la práctica de la arquitectura y su construcción.</p>
-				</div>
-				<div class="col-sm-7 pl-5"><img src="../images/index/index.png" class="big_img"></div>
-			</div>
-
-			<div class="row mt-0">
-				<div class="col-sm-5 mt-0">
-					<img class="img-fluid" src="https://via.placeholder.com/600x720">
-				</div>
-				<div class="col-sm-7 pl-5 mt-5">
-					<br>
-					<p class="lead mt-3 mr-5">Sumado a lo anterior, ofrecemos una red de trabajo que ayudará a vincularte con profesionistas activos en los temas descritos. Podrás leer sus perfiles y contactarlos para posibles colaboraciones.</p>
-					<br>
-					<p class="lead mt-3 mr-5">No olvides visitar nuestra sección de tienda para apoyar con la compra de artículos que nos ayudarán a mantener <i>Archtion Movements</i>.</p>
-					<br>
-					<p class="lead mt-3 mr-5 text-green font">¡Gracias por tu preferencia! Esperamos que encuentres valiosa la recopilación de información compartida en este sitio web.</p>
-				</div>
-				
-			</div>
-		</div>
-		<footer class="bg-dark text-center text-lg-start mt-3 text-white">
-			<div class="text-center p-3 bg-black">
-				<a href="index.php">
-					<img class="mt-4 logo-footer" src="../images/dark_logo.png">
-				</a>
-		        <div class="container mt-5 mb-3">
-				  	<div class="row">
-					    <div class="col-sm">
-					      	<a href="acerca_de.php">Acerca de</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="proyectos.php">Proyectos & obras</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Investigación</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Red de trabajo</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Eventos</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Tienda</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Contacto</a>
-					    </div>
-				  	</div>
-				</div>
-			</div>
-			<div class="text-center p-3 bg-gray" style="letter-spacing: 5px;">
-				<small>TODOS LOS DERECHOS RESERVADOS</small>
-			</div>
-		</footer>
-
-
-		<!-- JQuery Popper and Bootstrap-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-		<!-- NAVBAR JQUERY-->
-		<script src="../js/navbar.js"></script>
-	</body>
-=======
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>Arction Movements | Inicio</title>
+		<title>Arction Movements</title>
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<meta name="author" content="" />
@@ -168,15 +23,15 @@
 		    <div class="container mt-2">
 		        <div class="row">
 		            <div class="col-6">
-		                <a href="contacto.html">Contacto</a>
+		                <a href="contacto.php">Contacto</a>
 		            </div>
 		            <div class="col-6 text-right">
-		                <span class="mr-2"><a href="enviar_material.html">Enviar material</a> </span> | <span class="ml-2"> <a href="registro.html">Iniciar sesión</a></span>
+		                <span class="mr-2"><a href="enviar_material.php">Enviar material</a> </span> | <span class="ml-2"> <a href="registro.php">Iniciar sesión</a></span>
 		            </div>
 					
 		        </div>
 				<div class="text-center">
-					<a href="index.html">
+					<a href="index.php">
 						<img class="mt-4 logo" src="../images/logo.png">
 					</a>
 				</div>
@@ -189,16 +44,16 @@
         		</button>
 		        <div class="collapse navbar-collapse justify-content-center" id="navbar1">
 
-		        	<a class="navbar-brand" href="../pages/index.html">
+		        	<a class="navbar-brand" href="../pages/index.php">
 				    	<img id="navbar-logo" src="../images/small_logo.png" alt="Logo">
 				  	</a>
 
 		            <ul class="navbar-nav">
 		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="acerca_de.html"><u>Acerca de</u></a>
+		                    <a class="nav-link" href="acerca_de.php"><u>Acerca de</u></a>
 		                </li>
 		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="proyectos.html">Proyectos & Obras</a>
+		                    <a class="nav-link" href="proyectos.php">Proyectos & Obras</a>
 		                </li>
 		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="#">Investigación</a>
@@ -298,16 +153,16 @@
 		
 		<footer class="bg-dark text-center text-lg-start mt-3 text-white">
 			<div class="text-center p-3 bg-black">
-				<a href="index.html">
+				<a href="index.php">
 					<img class="mt-4 logo-footer" src="../images/dark_logo.png">
 				</a>
 		        <div class="container mt-5 mb-3">
 				  	<div class="row">
 					    <div class="col-sm">
-					      	<a href="acerca_de.html">Acerca de</a>
+					      	<a href="acerca_de.php">Acerca de</a>
 					    </div>
 					    <div class="col-sm">
-					      	<a href="proyectos.html">Proyectos & obras</a>
+					      	<a href="proyectos.php">Proyectos & obras</a>
 					    </div>
 					    <div class="col-sm">
 					      	<a href="">Investigación</a>
@@ -341,5 +196,4 @@
 		<!-- NAVBAR JQUERY-->
 		<script src="../js/navbar.js"></script>
 	</body>
->>>>>>> develop:public/pages/acerca_de.html
 </html>
--- public/pages/proyectos.php
+++ public/pages/proyectos.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:public/pages/proyectos.php
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 	<head>
@@ -22,19 +21,22 @@
 
 	</head>
 	<body>
-		<div class="header bg-light py-2">
+<div class="header bg-light py-2">
 		    <div class="container mt-2">
 		        <div class="row">
 		            <div class="col-6">
-		                CONTACTO
+		                <a href="contacto.php">Contacto</a>
 		            </div>
 		            <div class="col-6 text-right">
-		                <span class="mr-2">ENVIAR MATERIAL </span> | <span class="ml-2"> INICIAR SESIÓN</span>
+		                <span class="mr-2"><a href="enviar_material.php">Enviar material</a> </span> | <span class="ml-2"> <a href="registro.php">Iniciar sesión</a></span>
 		            </div>
+					
 		        </div>
-					<div class="text-center">
-					<img class="mt-4 logo" src="../images/logo.png">
-					</div>
+				<div class="text-center">
+					<a href="index.php">
+						<img class="mt-4 logo" src="../images/logo.png">
+					</a>
+				</div>
 		    </div>
 		</div>
 		<nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light underline">
@@ -49,22 +51,22 @@
 				  	</a>
 
 		            <ul class="navbar-nav">
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="acerca_de.php">Acerca de</a>
 		                </li>
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="proyectos.php"><u>Proyectos & Obras</u></a>
 		                </li>
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="#">Investigación</a>
 		                </li>
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="#">Red de trabajo</a>
 		                </li>
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="#">Eventos</a>
 		                </li>
-		                <li class="nav-item m-3 lead active">
+		                <li class="nav-item m-3 active">
 		                    <a class="nav-link" href="#">Tienda</a>
 		                </li>
 		            </ul>
@@ -238,248 +240,4 @@
 		<!-- NAVBAR JQUERY-->
 		<script src="../js/navbar.js"></script>
 	</body>
-=======
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>Arction Movements | Proyectos y Obras</title>
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="author" content="" />
-		<link rel="shortcut icon" href=""> 
-
-		<!-- BOOTSTRAP CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-		<!-- CSS -->
-		<link rel="stylesheet" type="text/css" href="../css/main.css">
-
-		 <!--CSS GRIDS-->
-		<link rel="stylesheet" type="text/css" href="../css/collage.css">
-
-	</head>
-	<body>
-		<div class="header bg-light py-2">
-		    <div class="container mt-2">
-		        <div class="row">
-		            <div class="col-6">
-		                <a href="contacto.html">Contacto</a>
-		            </div>
-		            <div class="col-6 text-right">
-		                <span class="mr-2"><a href="enviar_material.html">Enviar material</a> </span> | <span class="ml-2"> <a href="registro.html">Iniciar sesión</a></span>
-		            </div>
-					
-		        </div>
-				<div class="text-center">
-					<a href="index.html">
-						<img class="mt-4 logo" src="../images/logo.png">
-					</a>
-				</div>
-		    </div>
-		</div>
-		<nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light underline">
-		    <div class="container">
-		    	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar1">
-            <span class="navbar-toggler-icon"></span>
-        		</button>
-		        <div class="collapse navbar-collapse justify-content-center" id="navbar1">
-
-		        	<a class="navbar-brand" href="../pages/index.html">
-				    	<img id="navbar-logo" src="../images/small_logo.png" alt="Logo">
-				  	</a>
-
-		            <ul class="navbar-nav">
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="acerca_de.html">Acerca de</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="proyectos.html"><u>Proyectos & Obras</u></a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Investigación</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Red de trabajo</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Eventos</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Tienda</a>
-		                </li>
-		            </ul>
-		        </div>
-		    </div>
-		</nav>
-
-		<div class="container mt-4">
-
-			<!-- MENU -->
-			<div class="row mt-4 mb-4">
-				<div class="col-md-4 mt-2 adjust-text-center">
-					<h2 class="display-5 font-weight-bold">BUSCAR</h2>
-				</div>
-
-				<div class="col-md-3 bg-light">
-					<div class="bg-light">
-						<div class="dropdown">
-							<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Categoría
-							</a>
-						  
-							<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
-							  <a class="dropdown-item" href="#">Uno</a>
-							  <a class="dropdown-item" href="#">Dos</a>
-							  <a class="dropdown-item" href="#">Tres</a>
-							</div>
-						  </div>
-					</div>
-				</div>
-
-				<div class="col-md-3 bg-light">
-					<div class="bg-light">
-						<div class="bg-light">
-							<div class="dropdown">
-								<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								  Categoría
-								</a>
-							  
-								<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Uno</a>
-									<a class="dropdown-item" href="#">Dos</a>
-									<a class="dropdown-item" href="#">Tres</a>
-								</div>
-							  </div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col-md-3 ">
-					<div class="bg-dark">
-					</div>
-				</div>
-			</div>
-			<!-- CARDS -->
-			<div class="row">
-	            <div class="col-md-4 pr-1">
-	              	<div class="card mb-4 box-shadow hover_img">
-						<a href="publicacion.html">
-							<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-						</a>
-						<div class="mt-3">
-							<h5 class="card-title">Card title</h5>
-							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-						</div>
-	            	</div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-							<h5 class="card-title">Card title</h5>
-							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	        </div>
-		</div>
-
-		<footer class="bg-dark text-center text-lg-start mt-3 text-white">
-			<div class="text-center p-3 bg-black">
-				<a href="index.html">
-					<img class="mt-4 logo-footer" src="../images/dark_logo.png">
-				</a>
-		        <div class="container mt-5 mb-3">
-				  	<div class="row">
-					    <div class="col-sm">
-					      	<a href="acerca_de.html">Acerca de</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="proyectos.html">Proyectos & obras</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Investigación</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Red de trabajo</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Eventos</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Tienda</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Contacto</a>
-					    </div>
-				  	</div>
-				</div>
-			</div>
-			<div class="text-center p-3 bg-gray" style="letter-spacing: 5px;">
-				<small>TODOS LOS DERECHOS RESERVADOS</small>
-			</div>
-		</footer>
-
-		<script src="../js/modernizr.custom.js"></script>
-		<script src="../js/masonry.pkgd.min.js"></script>
-		<script src="../js/imagesloaded.js"></script>
-		<script src="../js/classie.js"></script>
-		<script src="../js/AnimOnScroll.js"></script>
-		<script>
-			new AnimOnScroll( document.getElementById( 'grid' ), {
-				minDuration : 0.4,
-				maxDuration : 0.7,
-				viewportFactor : 0.2
-			} );
-		</script>
-
-
-		<!-- JQuery Popper and Bootstrap-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-		<!-- NAVBAR JQUERY-->
-		<script src="../js/navbar.js"></script>
-	</body>
->>>>>>> develop:public/pages/proyectos.html
 </html>
