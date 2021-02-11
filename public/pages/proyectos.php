b9083a205fe4b6b5f0e9796896fc2a88269ee544
Bernardo Estrada <estradaf.bernardo@gmail.com>
2021-02-10 19:05:40 -0600

    Split pages into templates to improve modularity

--- public/pages/proyectos.php
+++ public/pages/proyectos.php
@@ -1,240 +1,125 @@
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>Arction Movements | Proyectos y Obras</title>
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<meta name="author" content="" />
-		<link rel="shortcut icon" href=""> 
+<?php include('../templates/_header.html') ?>
+<?php include('../templates/_navbar.html') ?>
 
-		<!-- BOOTSTRAP CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<div class="container mt-4">
 
-		<!-- CSS -->
-		<link rel="stylesheet" type="text/css" href="../css/main.css">
-
-		 <!--CSS GRIDS-->
-		<link rel="stylesheet" type="text/css" href="../css/collage.css">
-
-	</head>
-	<body>
-		<div class="header bg-light py-2">
-		    <div class="container mt-2">
-		        <div class="row">
-		            <div class="col-6">
-		                CONTACTO
-		            </div>
-		            <div class="col-6 text-right">
-		                <span class="mr-2">ENVIAR MATERIAL </span> | <span class="ml-2"> INICIAR SESIÓN</span>
-		            </div>
-		        </div>
-					<div class="text-center">
-					<img class="mt-4 logo" src="../images/logo.png">
-					</div>
-		    </div>
+	<!-- MENU -->
+	<div class="row mt-4 mb-4">
+		<div class="col-md-4 mt-2 adjust-text-center">
+			<h2 class="display-5 font-weight-bold">BUSCAR</h2>
 		</div>
-		<nav class="navbar navbar-expand-sm sticky-top navbar-light bg-light underline">
-		    <div class="container">
-		    	<button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbar1">
-            <span class="navbar-toggler-icon"></span>
-        		</button>
-		        <div class="collapse navbar-collapse justify-content-center" id="navbar1">
 
-		        	<a class="navbar-brand" href="../pages/index.php">
-				    	<img id="navbar-logo" src="../images/small_logo.png" alt="Logo">
-				  	</a>
-
-		            <ul class="navbar-nav">
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="acerca_de.php">Acerca de</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="proyectos.php"><u>Proyectos & Obras</u></a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Investigación</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Red de trabajo</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Eventos</a>
-		                </li>
-		                <li class="nav-item m-3 active">
-		                    <a class="nav-link" href="#">Tienda</a>
-		                </li>
-		            </ul>
-		        </div>
-		    </div>
-		</nav>
-
-		<div class="container mt-4">
-
-			<!-- MENU -->
-			<div class="row mt-4 mb-4">
-				<div class="col-md-4 mt-2 adjust-text-center">
-					<h2 class="display-5 font-weight-bold">BUSCAR</h2>
-				</div>
-
-				<div class="col-md-3 bg-light">
-					<div class="bg-light">
-						<div class="dropdown">
-							<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Categoría
-							</a>
-						  
-							<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
-							  <a class="dropdown-item" href="#">Uno</a>
-							  <a class="dropdown-item" href="#">Dos</a>
-							  <a class="dropdown-item" href="#">Tres</a>
-							</div>
-						  </div>
+		<div class="col-md-3 bg-light">
+			<div class="bg-light">
+				<div class="dropdown">
+					<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Categoría
+					</a>
+					
+					<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
+						<a class="dropdown-item" href="#">Uno</a>
+						<a class="dropdown-item" href="#">Dos</a>
+						<a class="dropdown-item" href="#">Tres</a>
 					</div>
-				</div>
-
-				<div class="col-md-3 bg-light">
-					<div class="bg-light">
-						<div class="bg-light">
-							<div class="dropdown">
-								<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								  Categoría
-								</a>
-							  
-								<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Uno</a>
-									<a class="dropdown-item" href="#">Dos</a>
-									<a class="dropdown-item" href="#">Tres</a>
-								</div>
-							  </div>
-						</div>
 					</div>
-				</div>
-
-				<div class="col-md-3 ">
-					<div class="bg-dark">
-					</div>
-				</div>
 			</div>
-			<!-- CARDS -->
-			<div class="row">
-	            <div class="col-md-4 pr-1">
-	              	<div class="card mb-4 box-shadow hover_img">
-						<a href="publicacion.php">
-							<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+		</div>
+
+		<div class="col-md-3 bg-light">
+			<div class="bg-light">
+				<div class="bg-light">
+					<div class="dropdown">
+						<a class="btn btn-lg btn-block text-center btn-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Categoría
 						</a>
-						<div class="mt-3">
-							<h5 class="card-title">Card title</h5>
-							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+						
+						<div class="dropdown-menu text-center w-100" aria-labelledby="dropdownMenuLink">
+							<a class="dropdown-item" href="#">Uno</a>
+							<a class="dropdown-item" href="#">Dos</a>
+							<a class="dropdown-item" href="#">Tres</a>
 						</div>
-	            	</div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-		                	 <h5 class="card-title">Card title</h5>
-		                	 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	            <div class="col-md-4 pr-1">
-	              <div class="card mb-4 box-shadow hover_img">
-	                <img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
-		                <div class="mt-3">
-							<h5 class="card-title">Card title</h5>
-							<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-		                </div>
-	                </div>
-	            </div>
-	        </div>
-		</div>
-
-		<footer class="bg-dark text-center text-lg-start mt-3 text-white">
-			<div class="text-center p-3 bg-black">
-				<a href="index.php">
-					<img class="mt-4 logo-footer" src="../images/dark_logo.png">
-				</a>
-		        <div class="container mt-5 mb-3">
-				  	<div class="row">
-					    <div class="col-sm">
-					      	<a href="acerca_de.php">Acerca de</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="proyectos.php">Proyectos & obras</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Investigación</a>
-					    </div>
-					    <div class="col-sm">
-					      	<a href="">Red de trabajo</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Eventos</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Tienda</a>
-					    </div>
-					    <div class="col-sm">
-					    	<a href="">Contacto</a>
-					    </div>
-				  	</div>
+						</div>
 				</div>
 			</div>
-			<div class="text-center p-3 bg-gray" style="letter-spacing: 5px;">
-				<small>TODOS LOS DERECHOS RESERVADOS</small>
+		</div>
+
+		<div class="col-md-3 ">
+			<div class="bg-dark">
 			</div>
-		</footer>
+		</div>
+	</div>
+	<!-- CARDS -->
+	<div class="row">
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+				<a href="publicacion.php">
+					<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				</a>
+				<div class="mt-3">
+					<h5 class="card-title">Card title</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+			<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				<div class="mt-3">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+			<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				<div class="mt-3">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+			<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				<div class="mt-3">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+			<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				<div class="mt-3">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4 pr-1">
+			<div class="card mb-4 box-shadow hover_img">
+			<img class="card-img-top" src="https://via.placeholder.com/500x300" alt="Card image cap">
+				<div class="mt-3">
+					<h5 class="card-title">Card title</h5>
+					<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
-		<script src="../js/modernizr.custom.js"></script>
-		<script src="../js/masonry.pkgd.min.js"></script>
-		<script src="../js/imagesloaded.js"></script>
-		<script src="../js/classie.js"></script>
-		<script src="../js/AnimOnScroll.js"></script>
-		<script>
-			new AnimOnScroll( document.getElementById( 'grid' ), {
-				minDuration : 0.4,
-				maxDuration : 0.7,
-				viewportFactor : 0.2
-			} );
-		</script>
+<script src="../js/modernizr.custom.js"></script>
+<script src="../js/masonry.pkgd.min.js"></script>
+<script src="../js/imagesloaded.js"></script>
+<script src="../js/classie.js"></script>
+<script src="../js/AnimOnScroll.js"></script>
+<script>
+	new AnimOnScroll( document.getElementById( 'grid' ), {
+		minDuration : 0.4,
+		maxDuration : 0.7,
+		viewportFactor : 0.2
+	} );
+</script>
 
-
-		<!-- JQuery Popper and Bootstrap-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-		<!-- NAVBAR JQUERY-->
-		<script src="../js/navbar.js"></script>
-	</body>
-</html>
+<?php include('../templates/_footer.html') ?>
